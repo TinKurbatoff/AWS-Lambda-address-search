7a8ad7cc8b59f7aa774d90a1c99e0463c7102d38
Constantine Kurbatoff <6782841@gmail.com>
2022-07-03 22:32:14 -0400

    Updated settings, checked datat deployment mode

--- ETL/etlData.ts
+++ ETL/etlData.ts
@@ -24,26 +24,13 @@ var addressesTableName = process.env?.ADDR_TABLE_NAME || 'addresses_default';
 export async function deployDataToDB(pool: PoolClient): Promise<string> {    
     // Create an empty table in database
     const result = await seedDatabase(addressesTableName);
-    // Read data to DB
-
-    
-    // data input values:
-    // const values = [{col_a: 'a1', col_b: 'b1'}, {col_a: 'a2', col_b: 'b2'}];
-        
-    // generating a multi-row insert query:
-    // const addRowsAtOnce = pgpromise.helpers.insert(values, cs);
-
-    // const addRowsAtOnce = `INSERT INTO $tableName
-    //                         (fips,apn,street_number,street_pre_direction,street_name,street_suffix
-    //                         ,street_post_direction,unit_type,unit_number,formatted_street_address
-    //                         ,city,state,zip_code,zip_plus_four_code,latitude,longitude
-    //                         ,geocoding_accuracy,census_tract,carrier_code)
-    //                         VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19) RETURNING id;`.replace('$tableName', addressesTableName)
-    
-    let x = 0
-    let linebuffer: Array<strObj> = []
-    let tableColumns: Array<string> = []
-    let tableColumnsArray: Array<string> = []
+    if (process.env.START_DEPLOY_DATA !== "True") { return "⚠️  NO DATA DEPLOYMENT MODE"}
+    // Read data into DB
+    let x = 0 // rows counter
+    let linebuffer: Array<strObj> = []  // Buffer for insert multiline
+    let tableColumns: Array<string> = []  // columns names in the special view
+    let tableColumnsArray: Array<string> = []  // just a list of columns names
+    // Calculate start time
     let startTime = performance.now()
     let timeElapsedOld = startTime
     fs.createReadStream(process.env.DATA_FILE_URI || './property.csv')
@@ -59,7 +46,7 @@ export async function deployDataToDB(pool: PoolClient): Promise<string> {
                   let params: strObj = {}; // object of values — {column1:value1,..})
                   tableColumnsArray.forEach((key:string, i) => params[key] = row[i]); // fill with actual values
                   linebuffer.push(params) // add to array of values 
-                  if (linebuffer.length == 500) {
+                  if (linebuffer.length == 50) {
                     // console.log(linebuffer.toString())  // *** Sanity check ***
                     /* Generating a multi-row insert query */
                     const addRowsAtOnce = pgpromise.helpers.insert(linebuffer, tableColumns) + ' RETURNING id';
@@ -67,21 +54,18 @@ export async function deployDataToDB(pool: PoolClient): Promise<string> {
                     let result = await dbHandlerClass.queryPool(pool, addRowsAtOnce, [])  
                     // console.log(`RESULT length:${result.length}`)
                     // Measure timeing 
-                    let timeElapsed = (performance.now() - startTime) / 1000
+                    let timeElapsed = (performance.now() - startTime) / 1000  // convert ms to seconds
                     if (timeElapsedOld < Math.floor(timeElapsed)) {
                       console.log(`Inserted ${x} lines | took ${timeElapsed.toFixed(6)} s`) 
                       }
                     timeElapsedOld = Math.floor(timeElapsed)
                     
-                  }
-                  
+                  }               
                 }
                 x++;
-                // console.log(x);
               })
             .on("end", function () {
                 console.log("finished");
-                // process.exit(0) //  *** DEBUG ***
               })
             .on("error", function (error) {
                 console.log(`Error on x:${x} line`)
